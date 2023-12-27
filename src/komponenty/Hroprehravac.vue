55b4f7ec2819fcd99d42cb28fb5a4a4a35a06920
GamingasCZ <gamingforyou875@gmail.com>
2023-12-27 16:02:31 +0100

    přehrávač her se adaptuje velikosti obrazovky

--- src/komponenty/Hroprehravac.vue
+++ src/komponenty/Hroprehravac.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import hry from '@/assety/hry';
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 
@@ -9,12 +9,21 @@ const hraIndex = computed(() => {
     return hry.findIndex(x => x.url == param)
 })
 
+const hra = ref<HTMLIFrameElement>()
+onMounted(() => {
+    hra.value?.addEventListener("load", () => {
+        hra.value.contentDocument.querySelector("canvas").style.maxWidth = "100vw"
+        hra.value.contentDocument.querySelector("canvas").style.maxHeight = "100vh"
+    })
+})
+
+
 const pr = ref(import.meta.env.BASE_URL)
 </script>
 
 <template>
-    <main class="mx-auto">
-        <iframe class="w-[1280px] border-4 border-geo-400 aspect-video" :src="`${pr}/${hry[hraIndex].url}/index.html`"></iframe>
+    <main class="mx-auto w-screen max-w-[92rem] mt-20">
+        <iframe ref="hra" class="mx-auto w-full max-w-5xl border-4 border-geo-400 aspect-video" :src="`${pr}/${hry[hraIndex].url}/index.html`"></iframe>
         <nav class="flex relative justify-between mt-8 w-full h-10">
             <h2 class="px-6 text-2xl font-bold">{{ hry[hraIndex].nazev }}</h2>
         </nav>
@@ -24,3 +33,12 @@ const pr = ref(import.meta.env.BASE_URL)
         <Sekce nadpis="Popis" v-if="hry[hraIndex].popis" :text="hry[hraIndex].popis" />
     </section>
 </template>
+
+<style>
+
+iframe canvas {
+    width: 100% !important;
+    height: 100% !important;
+}
+
+</style>
