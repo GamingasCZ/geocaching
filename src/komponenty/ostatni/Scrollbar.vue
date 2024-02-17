144c3490283ee9cb8b4d5e832c2f1009a348ea4e
GamingasCZ <gamingforyou875@gmail.com>
2024-02-17 17:54:39 +0100

    Lepší scrollbar

--- src/komponenty/ostatni/Scrollbar.vue
+++ src/komponenty/ostatni/Scrollbar.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import clanky from "@/ikony/clanky.svg";
-import { ref } from "vue";
+import logo from "@/ikony/scrollbar1.svg";
+import jakZacit from "@/ikony/scrollbar2.svg";
+import odkazy from "@/ikony/scrollbar3.svg";
+import obrazky from "@/ikony/scrollbar4.svg";
+import { inject, ref } from "vue";
 
 const emit = defineEmits<{
     (e: "goto", ind: number): void
@@ -18,22 +22,31 @@ const startTimeout = () => {
 
 document.body.addEventListener("mousemove", startTimeout)
 document.body.addEventListener("wheel", startTimeout)
+const currScroll = inject<number>("currScroll")
 
 </script>
 
 <template>
     <section
         :class="{'!translate-x-16': !shown}"
-        class="flex items-center fixed transition-transform translate-x-0 right-0 flex-col z-40 justify-between h-[35rem] top-32 mr-2">
-        <clanky @click="emit('goto', 0)"
-            class="w-8 h-24 rounded-full border-4 transition-transform scale-100 cursor-pointer hover:scale-105 active:scale-110 aspect-square border-geo-400" />
-        <hr class="w-1 h-full border-none bg-geo-400">
-        <clanky @click="emit('goto', 1)"
-            class="w-8 h-24 rounded-full border-4 transition-transform scale-100 cursor-pointer hover:scale-105 active:scale-110 aspect-square border-geo-400" />
-        <hr class="w-1 h-full border-none bg-geo-400">
-        <clanky @click="emit('goto', 2)"
-        class="w-8 h-24 rounded-full border-4 transition-transform scale-100 cursor-pointer hover:scale-105 active:scale-110 aspect-square border-geo-400" />
-        <hr class="w-1 h-full border-none bg-geo-400">
-        <clanky @click="emit('goto', 3)"
-        class="w-8 h-24 rounded-full border-4 transition-transform scale-100 cursor-pointer hover:scale-105 active:scale-110 aspect-square border-geo-400" />
+        class="flex items-center fixed transition-transform translate-x-0 right-0 flex-col z-40 justify-between h-[35rem] max-h-[75vh] top-1/2 -translate-y-1/2 mr-2">
+        <div :class="{'brightness-50 hue-rotate-30': currScroll != 0}" class="rounded-full border-4 transition scale-100 cursor-pointer aspect-square border-geo-400 hover:scale-105 active:scale-110">
+            <logo @click="emit('goto', 0)"
+                class="" />
+        </div>
+        <hr :class="{'brightness-50 hue-rotate-30': ![0,1].includes(currScroll!)}" class="w-1 h-full border-none transition bg-geo-400">
+        <div :class="{'brightness-50 hue-rotate-30': currScroll != 1}" class="rounded-full border-4 transition scale-100 cursor-pointer aspect-square border-geo-400 hover:scale-105 active:scale-110">
+            <jakZacit @click="emit('goto', 1)"
+                class="" />
+        </div>
+        <hr :class="{'brightness-50 hue-rotate-30': ![2, 1].includes(currScroll!)}" class="w-1 h-full border-none transition bg-geo-400">
+        <div :class="{'brightness-50 hue-rotate-30': currScroll != 2}" class="rounded-full border-4 transition scale-100 cursor-pointer aspect-square border-geo-400 hover:scale-105 active:scale-110">
+            <odkazy @click="emit('goto', 2)"
+                class="" />
+        </div>
+        <hr :class="{'brightness-50 hue-rotate-30': ![2, 3].includes(currScroll!)}" class="w-1 h-full border-none transition bg-geo-400">
+        <div :class="{'brightness-50 hue-rotate-30': currScroll != 3}" class="rounded-full border-4 transition scale-100 cursor-pointer aspect-square border-geo-400 hover:scale-105 active:scale-110">
+            <obrazky @click="emit('goto', 3)"
+                class="" />
+        </div>
 </section></template>
